17d5a629bf378b3d15f47c419d1629dc1e951cfa
nicolas9527 <122787122+nicolas9527@users.noreply.github.com>
2026-01-05 00:12:18 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,15 @@
-const cacheName = 'kiem-date-v1';
-const assets = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'date-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
